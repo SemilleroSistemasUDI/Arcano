e0e9af41f05de9c69fc234f0bb83eff70e15c7c7
John Alejandro Mantilla celis <alejandromantilla7@hotmail.com>
2015-10-03 16:55:27 -0500

    Actualizacion de la plataforma de desarrollo para la UI

--- Optimizacion/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-// stdafx.cpp : e utiliza para generar un archivo de encabezado precompilado denominado Win32.pch 
-// y un archivo de tipos precompilado denominado Stdafx.obj.
-// Optimizacion.pch sera un cabecera precompilada
-// stdafx.obj contendrá la información de tipo pre-compilado
-
-#include "stdafx.h"
-
-// TODO: Referencias adicionales para los encabezados en STDAFX.H
-// y no en este archivo
--- Optimizacion/targetver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Incluyendo SDKDDKVer.h define la plataforma más alta de Windows disponible.
-
-// Si desea crear su aplicación para una plataforma de Windows anterior, incluya WinSDKVer.h y
-// la macro _WIN32_WINNT de la la plataforma que desee apoyar antes de incluir SDKDDKVer.h.
-
-#include <SDKDDKVer.h>
