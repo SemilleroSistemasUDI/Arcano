9b292f4de2d79b2d88300e7444984e91fcd865ca
John Alejandro Mantilla celis <alejandromantilla7@hotmail.com>
2015-10-03 21:55:12 -0500

    Se agrego la licencia tipo BSD

--- Arcano/FormInicio.cs
+++ Arcano/FormInicio.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿#region Copyright Semillero Master Digital - GIDSAW. 2015-Presente.
+// Copyright Semillero Master Digital - GIDSAW. 2015-Presente. 
+// Todos los derechos reservados.
+// Este codigo fuente esta licenciado bajo la licencia con el estilo BSD y la puede encontrar 
+// en el archivo LICENSE en la raiz del proyecto. 
+//
+// =========================================================================================
+//
+// Copyright Semillero Master Digital - GIDSAW. 2015-Present. 
+// All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. 
+#endregion
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
