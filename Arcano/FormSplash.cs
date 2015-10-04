04b014d1eb1b6201ff4a0f4bf3ca75c82f000573
John Alejandro Mantilla celis <alejandromantilla7@hotmail.com>
2015-10-03 20:33:47 -0500

    Splash de la aplicacion, punto de entrada y validador de las configuraciones iniciales

---
+++ Arcano/FormSplash.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Windows.Forms.Tools;
+using Syncfusion.Windows.Forms;
+
+namespace Arcano
+{
+    public partial class FormSplash : MetroForm
+    {
+        public FormSplash()
+        {
+            InitializeComponent();
+        }
+    }
+}
