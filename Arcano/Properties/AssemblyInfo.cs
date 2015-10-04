7914b43f3c67d2486137eb376f936cc7fcf74f86
John Alejandro Mantilla celis <alejandromantilla7@hotmail.com>
2015-10-03 21:55:54 -0500

    Informacion para crear el ensamblado

--- Arcano/Properties/AssemblyInfo.cs
+++ Arcano/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Arcano")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Herramienta para procesar los logs de consumo del ancho de banda de una red.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Semillero Master Digital - GIDSAW")]
 [assembly: AssemblyProduct("Arcano")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
